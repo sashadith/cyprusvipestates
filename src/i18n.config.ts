97b47f8d2909a42ad326c689f7a91595478f7265
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-09-05 21:01:10 +0200

    fix default locale

--- src/i18n.config.ts
+++ src/i18n.config.ts
@@ -1,3 +1,6 @@
+import { notFound } from "next/navigation";
+import { getRequestConfig } from "next-intl/server";
+
 // Here we set English as the default language
 const languages = [
   { id: "en", title: "English", isDefault: true },
@@ -6,9 +9,20 @@ const languages = [
   { id: "pl", title: "Polish" },
 ];
 
-const i18n = {
+export const i18n = {
   languages,
   base: languages.find((item) => item.isDefault)?.id,
 };
 
-export { i18n };
+export const locales = languages?.map((el) => el.id);
+export const defaultLocale = languages?.find((el) => el.isDefault)?.id || "en";
+
+export default getRequestConfig(async ({ locale }) => {
+  if (!locales.includes(locale as any)) {
+    notFound();
+  }
+
+  return {
+    messages: undefined,
+  };
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,17 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+import createIntlMiddleware from "next-intl/middleware";
 
-import { i18n } from "@/i18n.config";
+import { defaultLocale, locales } from "@/i18n.config";
 
-import { match as matchLocale } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
+export default async function middleware(request: NextRequest) {
+  const handleI18nRouting = createIntlMiddleware({
+    locales,
+    defaultLocale,
+    localePrefix: "as-needed",
+    localeDetection: false,
+  });
 
-function getLocale(request: NextRequest): string | undefined {
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  const locales: string[] = i18n.languages.map((lang) => lang.id);
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-
-  const locale = matchLocale(languages, locales, i18n.base || "id");
-  return locale;
-}
-
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  const pathnameIsMissingLocale = i18n.languages.every(
-    (locale) =>
-      !pathname.startsWith(`/${locale.id}/`) && pathname !== `/${locale.id}`
-  );
-
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url
-      )
-    );
-  }
+  return handleI18nRouting(request);
 }
 
 export const config = {
