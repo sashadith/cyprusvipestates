fcb140def34654de043b5027964b59a034628ce6
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2025-02-21 23:34:00 +0100

    change map marker

--- src/app/components/PropertyMap/PropertyMap.tsx
+++ src/app/components/PropertyMap/PropertyMap.tsx
@@ -16,16 +16,24 @@ L.Icon.Default.mergeOptions({
 });
 
 // Создаем кастомный маркер с цветом #aa7f2e
-const customMarkerIcon = new L.DivIcon({
-  html: `
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="41" viewBox="0 0 25 41" fill="none">
-      <path d="M12.5 0C5.6 0 0 5.6 0 12.5C0 18.8 5.6 28.2 12.5 40.9C19.4 28.2 25 18.8 25 12.5C25 5.6 19.4 0 12.5 0ZM12.5 17.2C9.6 17.2 7.3 14.9 7.3 12C7.3 9.1 9.6 6.8 12.5 6.8C15.4 6.8 17.7 9.1 17.7 12C17.7 14.9 15.4 17.2 12.5 17.2Z" fill="#aa7f2e"/>
-    </svg>
-  `,
-  className: "", // Отключаем стандартные стили
-  iconSize: [25, 41], // Размер маркера
-  iconAnchor: [12.5, 41], // Точка привязки маркера к координатам
-  popupAnchor: [0, -41], // Точка привязки всплывающего окна
+// const customMarkerIcon = new L.DivIcon({
+//   html: `
+//     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="41" viewBox="0 0 25 41" fill="none">
+//       <path d="M12.5 0C5.6 0 0 5.6 0 12.5C0 18.8 5.6 28.2 12.5 40.9C19.4 28.2 25 18.8 25 12.5C25 5.6 19.4 0 12.5 0ZM12.5 17.2C9.6 17.2 7.3 14.9 7.3 12C7.3 9.1 9.6 6.8 12.5 6.8C15.4 6.8 17.7 9.1 17.7 12C17.7 14.9 15.4 17.2 12.5 17.2Z" fill="#aa7f2e"/>
+//     </svg>
+//   `,
+//   className: "", // Отключаем стандартные стили
+//   iconSize: [25, 41], // Размер маркера
+//   iconAnchor: [12.5, 41], // Точка привязки маркера к координатам
+//   popupAnchor: [0, -41], // Точка привязки всплывающего окна
+// });
+
+const customMarkerIcon = new L.Icon({
+  iconUrl:
+    "https://cdn.sanity.io/files/88gk88s2/production/ee440e32847adc482c7013426a18c4d5e50bc486.png", // замените на URL вашего изображения
+  iconSize: [70, 85], // размер изображения
+  iconAnchor: [12.5, 41], // точка привязки (середина нижней части)
+  popupAnchor: [0, -41], // положение всплывающего окна относительно маркера
 });
 
 type Props = {
