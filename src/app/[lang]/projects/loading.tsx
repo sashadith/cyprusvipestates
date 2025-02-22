db2af3760f5e99b91308161fe1947294677f988c
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2025-02-22 20:50:37 +0100

    edit loader

--- src/app/[lang]/projects/loading.tsx
+++ src/app/[lang]/projects/loading.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export default function Loading() {
   return (
     <div className="loader-wrapper">
       <span className="loader"></span>
+      <Image
+        src="https://cdn.sanity.io/files/88gk88s2/production/82c40f36d0c0cec712ca09a2c7149ac3c9b7dbf1.png"
+        alt="Cyprus VIP Estatees Logo"
+        width={400}
+        height={511}
+        className="loader-img"
+      />
     </div>
   );
 }
