b4e3107effcd275edae384f429d3a093b2a8a2d5
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2025-04-19 16:15:28 +0200

    create how we work block

--- src/app/components/HowWeWorkBlock/HowWeWorkBlock.tsx
+++ src/app/components/HowWeWorkBlock/HowWeWorkBlock.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
 import styles from "./HowWeWorkBlock.module.scss";
 import { HowWeWorkBlock as HowWeWorkBlockType } from "@/types/homepage";
+import Image from "next/image";
+import { urlFor } from "@/sanity/sanity.client";
+import { MdOutlineArrowCircleDown } from "react-icons/md";
 
 type Props = {
   work: HowWeWorkBlockType;
@@ -15,6 +18,32 @@ const HowWeWorkBlock: FC<Props> = ({ work }) => {
     <section className={styles.howWeWorkBlock}>
       <div className="container">
         {work.title && <h2 className="h2">{work.title}</h2>}
+        <div className={styles.steps}>
+          {work.steps.map((step, index) => (
+            <div key={step._key} className={styles.step}>
+              <div className={styles.icon}>
+                <Image
+                  alt={step.text}
+                  src={urlFor(step.icon).url()}
+                  width={80}
+                  height={80}
+                  className={styles.icon}
+                />
+              </div>
+              <div className={styles.text}>
+                {step.text && <p className={styles.title}>{step.text}</p>}
+              </div>
+              {index < work.steps.length - 1 && (
+                <MdOutlineArrowCircleDown
+                  size={35}
+                  color="#bd8948"
+                  className={styles.arrow}
+                  aria-hidden="true"
+                />
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
