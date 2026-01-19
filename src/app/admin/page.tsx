e5388c960a6326249f91f0f4f2bdcf5da7e527a6
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2026-01-19 18:50:24 +0100

    admin paths new

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamicImport from "next/dynamic";
-
-export const dynamic = "force-static";
-
-const Studio = dynamicImport(() => import("./Studio"), { ssr: false });
-
-export default function AdminPage() {
-  return <Studio />;
-}
